a5a8ba2754ee4689c10c19ab0da3d4df3c0c5c6c
nikunj-shah-akino <nikunj.shah@akinolabs.com>
2026-04-08 10:59:57 +0530

    chore: upgrade vite and plugin-react, and configure wss protocol for HMR

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, // allow external access
+    host: true,
     allowedHosts: ["akino3.akinolabs.io"],
     hmr: {
+      protocol: "wss",
       host: "akino3.akinolabs.io",
     },
   },
