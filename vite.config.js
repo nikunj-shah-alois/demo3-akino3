a329c94b60bdff084ed52f386567d5dea291ab73
nikunj-shah-akino <nikunj.shah@akinolabs.com>
2026-04-08 10:56:36 +0530

    feat: configure allowed hosts in vite dev server for akino3.akinolabs.io 2

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true, // allow external access
     allowedHosts: ["akino3.akinolabs.io"],
+    hmr: {
+      host: "akino3.akinolabs.io",
+    },
   },
 });
